9294ff665a5f8dec012a7070e79df90ee1e49166
LAB02 Research <contact@lab02-research.org>
2022-03-28 09:46:50 +0200

    Fix naming

---
+++ src/HASS.Agent.Satellite.Service/Extensions/AuthExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Serilog;
+
+namespace HASS.Agent.Satellite.Service.Extensions
+{
+    public static class AuthExtensions
+    {
+        /// <summary>
+        /// Checks whether the provided auth ID corresponds to the stored value (optionally, if any)
+        /// </summary>
+        /// <param name="authId"></param>
+        /// <param name="caller"></param>
+        /// <param name="emptyAllowed"></param>
+        /// <returns></returns>
+        public static bool CheckAuthId(this string authId, string caller, bool emptyAllowed = false)
+        {
+            var storedAuthId = Variables.ServiceSettings?.AuthId ?? string.Empty;
+
+            switch (emptyAllowed)
+            {
+                case false when string.IsNullOrEmpty(storedAuthId):
+                    Log.Warning("[AUTH] [{method}] Stored ID is empty, auth declined", caller);
+                    return false;
+
+                case true when string.IsNullOrEmpty(storedAuthId):
+                    return true;
+            }
+
+            if (authId != storedAuthId)
+            {
+                Log.Warning("[RPC] [{method}] Invalid auth ID", caller);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/HASS.Agent.Satellite.Service/Models/HomeAssistant/Commands/InternalCommands/PublishAllSensorsCommand.cs
@@ -0,0 +1,22 @@
+﻿using HASS.Agent.Shared.Models.HomeAssistant.Commands;
+using HASS.Agent.Satellite.Service.Sensors;
+
+namespace HASS.Agent.Satellite.Service.Models.HomeAssistant.Commands.InternalCommands
+{
+    internal class PublishAllSensorsCommand : InternalCommand
+    {
+        internal PublishAllSensorsCommand(string name = "PublishAllSensors", string? id = default) : base(name, "", id)
+        {
+            State = "OFF";
+        }
+
+        public override void TurnOn()
+        {
+            State = "ON";
+
+            SensorsManager.ResetAllSensorChecks();
+
+            State = "OFF";
+        }
+    }
+}
